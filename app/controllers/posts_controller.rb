04eb90ba8d378547883204c4fbfc87b31df9bfac
Gaurav Rewaliya <GauravRewaliya@gmail.com>
2023-09-09 17:17:54 +0530

    initilize controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def destroy
+  end
+
+  def new
+
+  end
+  def create
+    
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/register'
+  get 'users/login'
+  resources :posts ,only: [:index ,:new ,:create , :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
